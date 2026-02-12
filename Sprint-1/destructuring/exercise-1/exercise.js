1a7b800a2e4d78490a4d56bec7dc1742c80180d4
Mohsen Zamani <mohsenzamanist@gmail.com>
2026-02-12 23:39:33 +0000

    Apply changes asked by the reviewer

--- Sprint-1/destructuring/exercise-1/exercise.js
+++ Sprint-1/destructuring/exercise-1/exercise.js
@@ -6,10 +6,14 @@ const personOne = {
 
 // Update the parameter to this function to make it work.
 // Don't change anything else.
-function introduceYourself({ name, age, favouriteFood }) {
+function introduceYourself({
+  name = "NO_NAME_PROVIDED",
+  age = "NO_AGE_PROVIDED",
+  favouriteFood = "NO_FOOD_PROVIDED",
+} = {}) {
   console.log(
     `Hello, my name is ${name}. I am ${age} years old and my favourite food is ${favouriteFood}.`
   );
 }
 
-introduceYourself(personOne);
+introduceYourself();
