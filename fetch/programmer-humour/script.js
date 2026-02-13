08bf2d138502c50d287aff291cd87aeac6433d46
Mohsen Zamani <mohsenzamanist@gmail.com>
2026-02-13 00:52:22 +0000

    Apply changes commented by reviewer

--- fetch/programmer-humour/script.js
+++ fetch/programmer-humour/script.js
@@ -2,12 +2,15 @@ const fetchData = async () => {
   const url = `https://xkcd.now.sh/?comic=latest`;
   try {
     const response = await fetch(url);
+
+    if (!response.ok) throw new Error(`HTTP error: ${response.status}`);
+
     const data = await response.json();
     const imageEl = document.getElementById("image");
     imageEl.src = data.img;
     imageEl.alt = data.alt;
   } catch (error) {
-    console.log(`Error: ${error}`);
+    console.log(`Failed to fetch: ${error}`);
   }
 };
 
